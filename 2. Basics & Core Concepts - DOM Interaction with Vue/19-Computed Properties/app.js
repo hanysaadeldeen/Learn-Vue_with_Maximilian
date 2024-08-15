f800714e7194fb7e683b4ef1ccc6dee707368a7a
HanyMohamed <hsaad9231@gmail.com>
2024-08-15 23:09:16 +0300

    Add watcher

--- 2. Basics & Core Concepts - DOM Interaction with Vue/19-Computed Properties/app.js
+++ 2. Basics & Core Concepts - DOM Interaction with Vue/19-Computed Properties/app.js
@@ -3,22 +3,38 @@ const app = Vue.createApp({
     return {
       name: "",
       firstName: "",
+      lastname: "",
+      // fullname: "",
     };
+  },
+  watch: {
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = value + " " + "saad";
+    //   }
+    // },
+    // lastname(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = value + " " + this.name + " " + "saad";
+    //   }
+    // },
   },
   computed: {
     fullname() {
       console.log("mohamed");
-
       if (this.name === "") {
         return "";
       }
-      return this.name + " " + "mohamed";
+      return this.name + " " + this.lastname;
     },
   },
   methods: {
     inputText() {
       console.log("hay");
-
       if (this.firstName === "") {
         return "";
       }
