b9eea69bf41e9fddbca0d11c6391bd393b23528e
HanyMohamed <hsaad9231@gmail.com>
2024-09-01 11:16:59 +0300

    Add vue router and style navigation

---
+++ Routing/routing-01-starting-setup/src/main.js
@@ -0,0 +1,26 @@
+import { createApp } from 'vue';
+
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    // { path: '/', component: App },
+    {
+      path: '/teams',
+      component: TeamsList,
+    },
+    {
+      path: '/users',
+      component: UsersList,
+    },
+  ],
+});
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
