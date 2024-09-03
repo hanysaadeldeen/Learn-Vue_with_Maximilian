2911a9157b3cfc3255e922d420c4c0a20cd99749
HanyMohamed <hsaad9231@gmail.com>
2024-09-03 12:59:00 +0300

    start using Vuex

---
+++ VueX/vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <base-container title="Vuex"></base-container>
+  <section>
+    <h2>counter: {{ this.$store.state.count }}</h2>
+    <button @click="increment">Increment</button>
+  </section>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+
+export default {
+  components: {
+    BaseContainer,
+  },
+  data() {
+    return {
+      count: '',
+    };
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment');
+      console.log(this.$store.state.count);
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ VueX/vuex-01-starting-setup/src/store/test-counter.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex';
+
+// Create a new store instance.
+const store = createStore({
+  state() {
+    return {
+      count: 0,
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
+});
+export default store;
