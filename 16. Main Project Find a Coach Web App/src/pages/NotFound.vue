fca6ee124ba523711b2cad7874fca95909f12087
HanyMohamed <hsaad9231@gmail.com>
2024-09-22 19:58:12 +0300

    ✨ feat(project): complete All project with the latest NotFound page

--- 16. Main Project Find a Coach Web App/src/pages/NotFound.vue
+++ 16. Main Project Find a Coach Web App/src/pages/NotFound.vue
@@ -1,5 +1,11 @@
 <template lang="">
-  <div>Page Not Found</div>
+  <section>
+    <base-card>
+      <h2>Page Not Found</h2>
+      <p>This page could not be found</p>
+      <router-link to="/coaches">coaches</router-link>
+    </base-card>
+  </section>
 </template>
 <script>
 export default {};
