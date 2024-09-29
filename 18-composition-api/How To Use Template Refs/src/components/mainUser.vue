42cafae2834f9f7220c1cbe36a42119d4a17426f
HanyMohamed <hsaad9231@gmail.com>
2024-09-29 11:03:42 +0300

    ✨feat(composition): access data using ref and props

---
+++ 18-composition-api/How To Use Template Refs/src/components/mainUser.vue
@@ -0,0 +1,19 @@
+<template lang="">
+  <h2>{{ userName }}</h2>
+  <h3>{{ age }}</h3>
+</template>
+<script>
+import { computed } from 'vue';
+export default {
+  props: ['firstName', 'lastName', 'age'],
+  setup(props) {
+    const userName = computed(() => {
+      return props.firstName + ' ' + props.lastName;
+    });
+    return {
+      userName,
+    };
+  },
+};
+</script>
+<style lang=""></style>
